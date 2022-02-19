7d737db6cb49852723067f85391166d7f371b63a
Francesco Lässig <flaessig@student.ethz.ch>
2022-02-19 19:19:05 +0100

    added primitive touchscreen controls

--- sketch.js
+++ sketch.js
@@ -8,6 +8,14 @@ function preload() {
   sprites_png = loadImage("data/character_sprites.png")
 }
 
+let ctrl_buttons = [
+    new RectButton(dir_button_x_offset, dir_button_y_offset + dir_button_dim, dir_button_dim, dir_button_dim, key_action, 'a'),
+    new RectButton(dir_button_x_offset + dir_button_dim, dir_button_y_offset, dir_button_dim, dir_button_dim, key_action, 'w'),
+    new RectButton(dir_button_x_offset + dir_button_dim, dir_button_y_offset + 2 * dir_button_dim, dir_button_dim, dir_button_dim, key_action, 's'),
+    new RectButton(dir_button_x_offset + 2 * dir_button_dim, dir_button_y_offset + dir_button_dim, dir_button_dim, dir_button_dim, key_action, 'd'),
+    new RectButton(dir_button_x_offset + 5 * dir_button_dim, dir_button_y_offset + 2 * dir_button_dim, dir_button_dim * 4, dir_button_dim, key_action, ' ')
+]
+
 function setup() {
 
   createCanvas(600, 450);
@@ -43,6 +51,13 @@ function setup() {
   
 
 function draw() {
+    if (touches.length > 0) {
+        draw_ctrl_buttons()
+        for (var i = 0; i < touches.length; i++) {
+            touch_action(touches[i])
+        }
+    }
+
     if (counter % 5 == 0 && pressed_key != null) {
         key_action(pressed_key)
     }   
@@ -55,6 +70,18 @@ function draw() {
     }
 
     counter += 1
+}
+
+function touch_action(touch) {
+    for (var i = 0; i < ctrl_buttons.length; i++) {
+        ctrl_buttons[i].try_press(touch)
+    }  
+}
+
+function draw_ctrl_buttons() {
+    for (var i = 0; i < ctrl_buttons.length; i++) {
+        ctrl_buttons[i].draw_button()
+    }  
 }
 
 function key_action(key) {
